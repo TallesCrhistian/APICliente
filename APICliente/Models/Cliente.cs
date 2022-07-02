caf0ea87091f0bf40c0b0535ca5e86654e79aa85
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-07-01 23:34:25 -0300

    Adicionar arquivos de projeto.

---
+++ APICliente/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICliente.Models
+{
+    public class Cliente
+    {
+        [Required (ErrorMessage = "Cpf é obriagtório!")]
+        public string Cpf { get; set; }
+
+        [Required (ErrorMessage = "Nome é obriagtório")]
+        public string Nome { get; set; }
+
+        [Required (ErrorMessage ="Data de Nascimento é obrigatório")]
+        public DateTime DataNascimento { get; set; }
+    }
+}
